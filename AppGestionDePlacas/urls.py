22bdc43982f87fff56ef63e02e04e520611d1355
profeduardo67cap <linuxsoft@gmail.com>
2023-12-09 19:16:09 -0600

    Creo una url de prueba

--- AppGestionDePlacas/urls.py
+++ AppGestionDePlacas/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from CatalogoDePlacas import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.helloworld),
 ]
