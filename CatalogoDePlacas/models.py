b09d25c2695ddffec92b0a9e97c84736373375b6
profeduardo67cap <linuxsoft@gmail.com>
2023-12-10 13:03:36 -0600

    Se crea el modelo para oficina

--- CatalogoDePlacas/models.py
+++ CatalogoDePlacas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Oficina(models.Model):
+    nombre = models.CharField(max_length=40)
+    ciudad = models.CharField(max_length=30)
+    telefono = models.CharField(max_length=15)
+    responsable = models.CharField(max_length=40, null=True)
+    cp = models.CharField(max_length=5, default=0)
+    
+    def __str__(self):
+        return "%s" % (self.nombre)
