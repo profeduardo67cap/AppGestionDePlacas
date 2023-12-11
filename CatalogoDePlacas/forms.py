4bcfb0d5f4e108974e61c744128c2a005befa77b
profeduardo67cap <linuxsoft@gmail.com>
2023-12-11 17:36:07 -0600

    Se agrega un campo en ña ñosta de campos para autogenerar el formulario en base al modelo

--- CatalogoDePlacas/forms.py
+++ CatalogoDePlacas/forms.py
@@ -4,4 +4,4 @@ from .models import Oficina
 class OficinaForm(ModelForm):
     class Meta:
         model = Oficina
-        fields = ['nombre', 'ciudad', 'telefono','responsable','cp']
+        fields = ['oficinaId','nombre', 'ciudad', 'telefono','responsable','cp']
